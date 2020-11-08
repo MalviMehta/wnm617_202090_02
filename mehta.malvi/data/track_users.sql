0c9d6bebe516105be9afbd985aac99e677746f46
Malvi Mehta <malvi@malvis-mbp.lan>
2020-11-07 17:57:00 -0800

    Updating tables

---
+++ mehta.malvi/data/track_users.sql
@@ -0,0 +1,11 @@
+INSERT INTO track_users VALUES
+(1,'Chrystal Richardson','user1','user1@gmail.com',md5("pass"),'+1 (841) 464-3196','female','https://via.placeholder.com/400/858/fff/?text=user1','2020-05-18 04:47:05'),
+(2,'Desiree Rollins','user2','user2@gmail.com',md5("pass"),'+1 (950) 599-3726','female','https://via.placeholder.com/400/922/fff/?text=user2','2020-06-06 08:30:31'),
+(3,'Zamora Harrison','user3','user3@gmail.com',md5("pass"),'+1 (876) 505-3066','male','https://via.placeholder.com/400/758/fff/?text=user3','2020-05-26 04:49:57'),
+(4,'Soto Schultz','user4','user4@gmail.com',md5("pass"),'+1 (816) 444-3942','male','https://via.placeholder.com/400/798/fff/?text=user4','2020-09-15 09:30:01'),
+(5,'Langley Bass','user5','user5@gmail.com',md5("pass"),'+1 (952) 479-2369','male','https://via.placeholder.com/400/920/fff/?text=user5','2020-04-02 01:02:55'),
+(6,'Nieves Arnold','user6','user6@gmail.com',md5("pass"),'+1 (878) 412-3566','male','https://via.placeholder.com/400/772/fff/?text=user6','2020-08-19 10:38:51'),
+(7,'Deborah Sherman','user7','user7@gmail.com',md5("pass"),'+1 (986) 548-2617','female','https://via.placeholder.com/400/827/fff/?text=user7','2020-05-12 09:35:03'),
+(8,'Shana Weber','user8','user8@gmail.com',md5("pass"),'+1 (955) 481-3029','female','https://via.placeholder.com/400/965/fff/?text=user8','2020-06-19 05:54:23'),
+(9,'Jackson Dillard','user9','user9@gmail.com',md5("pass"),'+1 (929) 525-3854','male','https://via.placeholder.com/400/733/fff/?text=user9','2020-07-27 12:56:31'),
+(10,'Dorothy Mckenzie','user10','user10@gmail.com',md5("pass"),'+1 (987) 510-3961','female','https://via.placeholder.com/400/972/fff/?text=user10','2020-09-13 06:14:23');
